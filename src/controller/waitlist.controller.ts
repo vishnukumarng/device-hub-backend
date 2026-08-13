ac0675bd74db541ab08b6019751070252c253a05
vishnukumar-dci <vishnukumar.ganeshbabu@dci.in>
2026-08-13 11:22:43 +0530

    type defined

--- src/controller/waitlist.controller.ts
+++ src/controller/waitlist.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import * as waitlistService from "../services/waitlist.service";
 import { successResponse } from "../utils/response";
 import { JoinWaitlistRequest } from "../types/waitlist.type";
+import { BadRequestError } from "../utils/AppError";
 
 export const join = async (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -24,6 +25,9 @@ export const leave = async (
 ) => {
   try {
     const { id } = req.params;
+    if (typeof id !== "string") {
+      throw new BadRequestError("Waitlist entry ID must be a string");
+    }
     const entry = await waitlistService.leaveWaitlist(id);
 
     return successResponse(res, 200, "Removed from waitlist", entry);
